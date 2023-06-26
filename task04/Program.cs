3760fcc61ca7325e56f1e278b63bf1a93bf06207
Aleksandr <a.suslov631@gmail.com>
2023-06-26 21:12:39 +0200

    Add folder 04

---
+++ task04/Program.cs
@@ -0,0 +1,12 @@
+﻿/*task04
+Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
+*/
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int integer = 0;
+
+if (number < 100 || number > 999) Console.WriteLine("Трехзначное число");
+else {
+    integer = number % 10;
+    Console.WriteLine(integer);
+}
