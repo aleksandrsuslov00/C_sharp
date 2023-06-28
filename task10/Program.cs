b4b22527b41ff09998d7f0261a54f7c6a4257c00
Aleksandr <a.suslov631@gmail.com>
2023-06-28 17:46:08 +0200

    task10_3

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 1, 
+    ya = 1, 
+    xb = 1, 
+    yb = 15,
+    xc = 45,
+    yc = 15;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");   
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
