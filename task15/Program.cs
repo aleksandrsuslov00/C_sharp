e5d8d2fc0063aafcc96709b6715e9e1b369f4e26
Aleksandr <a.suslov631@gmail.com>
2023-07-03 01:16:17 +0200

    new

---
+++ task15/Program.cs
@@ -0,0 +1,11 @@
+﻿int randomNumber = new Random().Next(10, 100);
+Console.WriteLine($"Рандомное число - {randomNumber}");
+int numberOne = randomNumber % 10;
+int numberTwo = randomNumber / 10;
+Console.Write($"У числа {randomNumber} большая цифра - ");
+
+if (numberOne > numberTwo)
+{
+    Console.Write(numberOne);
+}
+else {Console.Write(numberTwo);}
---
+++ task17/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая будет принимать на вход 
+два числа и выводить, является ли второе 
+число кратно первому. Если второе число не кратно первому, 
+то программа выводит остаток деления.
+34, 5 -> некратно, остаток 4
+16, 4 -> кратно
+*/
+Console.Write("Введите 1 число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int result = firstNumber % secondNumber;
+if (result == 0) 
+    Console.WriteLine($"кратно");
+else
+    Console.WriteLine($"остаток {result}");
+
+
